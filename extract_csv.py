02427e5df2df786994bf9ee367e556751f4ff8d3
kli210 <87552132+kli210@users.noreply.github.com>
2023-02-16 20:10:35 -0500

    added new comments

--- extract_csv.py
+++ extract_csv.py
@@ -1,4 +1,4 @@
-#automate the download of global covid-19 dataset in csv file from the Our World in Data website
+# Automate the download of global covid-19 dataset in csv file from the Our World in Data website
 import urllib.request
 import time
 
@@ -7,8 +7,9 @@ filename = "covid_data.csv"
 
 while True:
     urllib.request.urlretrieve(url, filename)
-    print("File successfully downloaded!")
-    time.sleep(86400)  # waits for 24 hours before downloading again
-    
+    print("csv file successfully downloaded!")
+    time.sleep(86400)  # Waits for 24 hours before downloading again
+
+# Downloads up-to-date csv file daily   
 
 
